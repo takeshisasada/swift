130181e2915a8b83e1583ef1d137395636e37acf
Takeshi Sasada <takeshi@sasadataisho-no-MacBook-Air.local>
2015-07-03 22:56:33 +0900

    最初のコミット

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Takeshi Sasada on 2015/07/03.
+//  Copyright (c) 2015年 Takeshi Sasada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let a="test!"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
